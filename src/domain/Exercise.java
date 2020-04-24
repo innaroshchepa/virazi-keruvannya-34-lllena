474d5d45527ffea9639416ef5b5a70e625d06f0f
lllena <51027280+lllena@users.noreply.github.com>
2020-04-24 18:20:52 +0300

    Відредагувала код відповідно до завдання

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,38 @@
 package domain;
 
+/**
+ * Main class
+ * @author admin
+ */
+
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+ /**
+  * Main method for cheking if a given number is Armstrong`s number
+  * @param Number
+  * @return 
+  */   
+public static boolean Calculate(int Number) {
+    int count=0;              //variable for the number of digits
+    int num=Number;           //variable that store copies of the Number
+    int Arm=0;                //variable to check for Armstrong number
+    //create an array for 9 element
+    int[] digits = new int[9];
+    
+    //fill the array and counting the number of digits in a number
+    while(Number>0){
+         digits[count]=Number%10;
+         Number=Number/10;
+         count++;
+     }
+    
+    //we carry out mathematical operations
+    for(int i=0;i<count;i++){
+        Arm+=Math.pow(digits[i], count);
     }
+    
+    //check to belonging to Armstrong and return the result
+    return num==Arm;
+}
+//end of method Calculate()
+
 }
